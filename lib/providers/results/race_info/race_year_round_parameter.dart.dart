f21e9989563e85f6670cdf118ab13cec0cc45a40
DaniilPavlov <cool.sharagin@mail.ru>
2023-05-18 19:54:03 +0300

    some info about equatable

--- lib/providers/results/race_info/race_year_round_parameter.dart.dart
+++ lib/providers/results/race_info/race_year_round_parameter.dart.dart
@@ -4,11 +4,20 @@ import 'package:equatable/equatable.dart';
 // For families to work correctly, it is critical for the parameter passed to a
 // provider to have a consistent hashCode and ==.
 
+
+// TODO(info): позволяет добавлять equatable если мы extend'им что-то другое
+// class RaceYearRoundParameter with EquatableMixin {
+
 class RaceYearRoundParameter extends Equatable {
   final List<String> yearRound;
 
+// TODO(info): поля, которые находится в скобках, проверяется на равенство
   @override
   List<Object?> get props => [yearRound];
+
+// TODO(info): автоматически помогает отдавать стринговое представление объекта, а не просто instance
+  @override
+  bool get stringify => true;
 
   const RaceYearRoundParameter({required this.yearRound});
 }
