ab30b64b91f071ce875c999464cfb5a50e387d02
DaniilPavlov <cool.sharagin@mail.ru>
2023-05-24 12:46:09 +0300

    guide for github pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:f1_pet_project/app.dart';
 import 'package:flutter/material.dart';
 
-
+// TODO(info): guide for deploying to GitHub Pages
+// https://maheshmnj.medium.com/deploying-your-flutter-webapp-to-github-pages-111ff9e5cbc9
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
