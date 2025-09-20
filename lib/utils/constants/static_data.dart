ab8cc616038a462133a9d178d106886ab134fbc1
Daniil <pavlov.dv22@gmail.com>
2025-09-20 18:53:13 +0300

    feat(ios): updated ios

--- lib/utils/constants/static_data.dart
+++ lib/utils/constants/static_data.dart
@@ -2,5 +2,6 @@ class StaticData {
   static const defaultHorizontalPadding = 12.0;
   static const defaultVerticalPadding = 20.0;
 
+  // TODO(info): repository of this API: https://github.com/jolpica/jolpica-f1
   static const apiUrl = 'http://api.jolpi.ca/ergast/f1/';
 }
